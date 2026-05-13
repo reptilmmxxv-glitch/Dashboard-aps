4790c3c9832d8ad3c39036fb154498dfcf14405a
Camilo <camilo@example.com>
2026-05-13 17:04:11 -0400

    Carga y visualizacion de archivos

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Dashboard APS")
 
-st.write("Proyecto de gestión pública y análisis de datos")
+archivo = st.file_uploader(
+    "Sube un archivo Excel o CSV",
+    type=["csv", "xlsx"]
+)
+
+if archivo is not None:
+
+    if archivo.name.endswith(".csv"):
+        df = pd.read_csv(archivo)
+
+    else:
+        df = pd.read_excel(archivo)
+
+    st.subheader("Vista previa de datos")
+    st.dataframe(df)
